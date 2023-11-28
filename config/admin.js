bac51b285125a7aca4158051a41f70c01fc39202
AbdulRazak Abubakari <abdulrazakneate@gmail.com>
2023-11-28 11:56:43 +0000

    added config middleswares , cloudinary provider

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','oMHnHDvwBcU/O3usB6gMGA=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT','LZVn+Hhx/aQSowXXhVbcKg=='),
   },
 });
