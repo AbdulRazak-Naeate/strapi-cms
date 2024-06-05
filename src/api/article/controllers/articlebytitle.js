45a6cec6503332036fa9841c3b906981359f81a2
AbdulRazak Abubakari <abdulrazakneate@gmail.com>
2024-06-05 08:47:03 +0000

    fixing not populate * error

--- src/api/article/controllers/articlebytitle.js
+++ src/api/article/controllers/articlebytitle.js
@@ -9,15 +9,21 @@ const { createCoreController } = require('@strapi/strapi').factories;
 module.exports = createCoreController('api::article.article', ({ strapi }) => ({
     // Method to fetch an article by title
     async findOneByTitle(ctx) {
+
       const { title,populate } = ctx.query;
+      // Define the fields to populate
+    const populateFields = 
+    ['description','image', 'date_created','categories','title','author','likes','attributes']; 
+
       console.log(title)
       const entity = await strapi.db.query('api::article.article').findOne({
         
         where: { title },
-        populate:{populate}
+        populate:populateFields
       });
-  
+      
       if (!entity) {
+
         return ctx.notFound('Article not found');
       }
   
